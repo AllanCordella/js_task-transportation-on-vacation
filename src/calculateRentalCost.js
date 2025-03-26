3a1d85bbd7b63ccc7826192a77ae62bdfc6da36f
Allan Cordella <allancordella@gmail.com>
2025-03-26 18:22:43 -0300

    vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
+  const DAY_PRICE = 40;
+  const LONG_TIME = 7;
+  const LONG_TIME_DISCOUNT = 50;
+  const SHORT_TIME = 3;
+  const SHORT_TIME_DISCOUNT = 20;
 
-  const basePrice = days * PRICE_PER_DAY;
+  const basePrice = days * DAY_PRICE;
 
-  if (days >= LONG_TERM) {
-    return basePrice - LONG_TERM_DISCOUNT;
+  if (days >= LONG_TIME) {
+    return basePrice - LONG_TIME_DISCOUNT;
   }
 
-  if (days >= SHORT_TERM) {
-    return basePrice - SHORT_TERM_DISCOUNT;
+  if (days >= SHORT_TIME) {
+    return basePrice - SHORT_TIME_DISCOUNT;
   }
 
   return basePrice;
